4b6369d7eefff4b000e79d27af050602ba1b203f
Shiyue Wang <shiyue.wang@me.com>
2012-10-28 19:29:44 +0100

    Add fonts

--- Javascript/footerPage.js
+++ Javascript/footerPage.js
@@ -5,13 +5,13 @@ function loadPage() {
 		var pagetitle = "CONDITIONS G&Eacute;N&Eacute;RALES<div class='underLine'></div>";
 		document.getElementsByClassName('pagetitle')[0].innerHTML = pagetitle;
 		
-		var contentText = "Repelibuscium dolorum rendae<br>"
+		var contentText = "<b>Repelibuscium</b> dolorum rendae<br>"
 		+"post utes auttem renimi,<br>" 
 		+"olum sequatectem<br>"
 		+"et init quo quat<br>"
 		+" laborum apieni dolo ex eume porest,<br><br>"
 		+"occatis eaque aut<br><br>"
-		+"apid et lit ea verae rerio et et reium faci dolore niatincto bea volorehendi unt, sunto quos sous rendunt lament et ut aut quia voluptibea qui ut ut venihit endanie ntest, auta nullabore pa dusaeria iligendi tendeli quiatest untum hil im ut eaturep raepero cus cus, quid esti is ?;";
+		+"apid et lit ea verae rerio et et reium faci dolore niatincto bea volorehendi unt, sunto quos sous rendunt lament et ut aut quia voluptibea qui ut ut venihit endanie ntest, auta nullabore pa dusaeria iligendi tendeli quiatest untum hil im ut eaturep raepero cus cus, quid esti is ?";
 		document.getElementsByClassName('contentText')[0].innerHTML = contentText;
 	} 
 	else if ( idPage == "ml") 
@@ -19,13 +19,13 @@ function loadPage() {
 		var pagetitle = "MENTIONS L&Eacute;GALES<div class='underLine'></div>";
 		document.getElementsByClassName('pagetitle')[0].innerHTML = pagetitle;
 		
-		var contentText = "Repelibuscium dolorum rendae<br>"
+		var contentText = "<b>Repelibuscium</b> dolorum rendae<br>"
 		+"post utes auttem renimi,<br>" 
 		+"olum sequatectem<br>"
 		+"et init quo quat<br>"
 		+" laborum apieni dolo ex eume porest,<br><br>"
 		+"occatis eaque aut<br><br>"
-		+"apid et lit ea verae rerio et et reium faci dolore niatincto bea volorehendi unt, sunto quos sous rendunt lament et ut aut quia voluptibea qui ut ut venihit endanie ntest, auta nullabore pa dusaeria iligendi tendeli quiatest untum hil im ut eaturep raepero cus cus, quid esti is ?;";
+		+"apid et lit ea verae rerio et et reium faci dolore niatincto bea volorehendi unt, sunto quos sous rendunt lament et ut aut quia voluptibea qui ut ut venihit endanie ntest, auta nullabore pa dusaeria iligendi tendeli quiatest untum hil im ut eaturep raepero cus cus, quid esti is ?";
 		document.getElementsByClassName('contentText')[0].innerHTML = contentText;
 	}
 }
